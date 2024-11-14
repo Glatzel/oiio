03fa20fe10052cd5038cbe0c7546fb620afdc13d
Glatzel <893016099@qq.com>
2024-11-14 10:00:33 +0800

    build: use dynamic link

--- scripts/copy-item.ps1
+++ scripts/copy-item.ps1
@@ -4,6 +4,5 @@ Set-Location ..
 Remove-Item openimageio -Force -Recurse -ErrorAction SilentlyContinue
 
 new-item openimageio -itemtype directory
-copy-Item ./scripts/install-oiio.ps1 ./openimageio
 copy-Item ./oiio_dep/vcpkg_installed/x64-windows/bin/*.dll ./openimageio
 copy-Item ./external/OpenImageIO/dist/bin/* ./openimageio
