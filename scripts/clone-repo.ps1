9e214c62a153a88cc546f4e04b4e343ac5d0d4dd
Glatzel <893016099@qq.com>
2024-12-03 09:05:14 +0800

    fix

--- scripts/clone-repo.ps1
+++ scripts/clone-repo.ps1
@@ -15,8 +15,15 @@ Write-Output "::group::clone OpenImageIO"
 git clone https://github.com/AcademySoftwareFoundation/OpenImageIO.git
 Set-Location OpenImageIO
 git checkout tags/"$version" -b "$version-branch"
-# apply patch
+ # apply patch
 git apply -v $patch_libraw
+# copy dependencies to install dir
+"install(FILES $<TARGET_RUNTIME_DLLS:iconvert> TYPE BIN)" >> CmakeLists.txt 
+"install(FILES $<TARGET_RUNTIME_DLLS:idiff> TYPE BIN)" >> CmakeLists.txt 
+"install(FILES $<TARGET_RUNTIME_DLLS:igrep> TYPE BIN)" >> CmakeLists.txt 
+"install(FILES $<TARGET_RUNTIME_DLLS:iinfo> TYPE BIN)" >> CmakeLists.txt 
+"install(FILES $<TARGET_RUNTIME_DLLS:maketx> TYPE BIN)" >> CmakeLists.txt 
+"install(FILES $<TARGET_RUNTIME_DLLS:oiiotool> TYPE BIN)" >> CmakeLists.txt 
 Set-Location ..
 Write-Output "::endgroup::"
 
